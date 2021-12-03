7f803ae4adf6c54c5d8916e0c4cc12dd144e2588
dustinusey <dustinusey@me.com>
2021-12-03 06:50:28 -0600

    code comments, refactor, cleanup (js)

--- app.js
+++ app.js
@@ -1,62 +1,82 @@
+// overlay object
+// ** contains 1 function / handleStart
 const overlay = {
     handleStart: () => {
+        // variables
         const overlay = document.querySelector('.overlay');
         const heading = overlay.querySelector('h1');
         const p = overlay.querySelector('p');
         const btn = overlay.querySelector('button.overlay-btn');
+        // overlay event listener to test for button clicked
         overlay.addEventListener('click', e => {
             if (e.target.textContent === `Let's Play` || e.target.textContent === 'Play Again?') {
+                // calls getRandomPhrase from gameboard object
                 gameboard.getRandomPhrase();
+                // calls addPhraseToDisplay from gameboard object
                 gameboard.addPhraseToDisplay();
+                // transitions applied via css to overlay elements
                 heading.classList.add('hide');
                 p.classList.add('hide');
                 p.textContent = 'Good Luck!'
                 btn.classList.add('hide');
+                // 1.2second timeout for overlay exit
                 setTimeout(() => {
                     overlay.classList.remove('show');
                     p.style.display = 'none';
                 }, 1200);
-            } 
+            }
+            // condition for win/lose overlays that calls the restGame function
             if (e.target.textContent === 'Play Again?') {
                 resetGame();
             }
-            
         });
     }
 }
 
+// theme object
+// ** contains 1 function / handleThemeToggle
 const theme = {
+    // function to handle theme change from light to dark
     handleThemeToggle: () => {
+        // variables
         const themeContainer = document.querySelector('.theme-toggle');
         const light = themeContainer.querySelector('.light');
         const dark = themeContainer.querySelector('.dark');
+        // event listener on theme div to toggle clicked theme option
         themeContainer.addEventListener('click', e => {
             if (e.target === light) {
                 document.body.classList = '';
-            } else {
+            } else if (e.target === dark ) {
                 document.body.classList.add('whoturnedoffthelights')
             }
         })
     }
 }
 
+// gameboard object
+// ** holds information and logic about the gameboard (keyboard in another object)
+// ** 2 functions - getRandomPhrase & addPhraseToDisplay
 const gameboard = {
+    // keys/values
     lives: 5,
-    phrases: [
-        'nubcake for life',
-        'need more coffee',
-        'console dot log it',
-        'bugs everywhere',
-        'stack overflow it',
-        'ole guily guily'
-    ],
+    phrases: ['nubcake for life','need more coffee','console dot log it','bugs everywhere','stack overflow it','ole guily guily'],
     selectedPhrase: "",
+    // getRandomPhrase function
+    // ** takes the gameboard.phrases array as an arg
+    // ** gets a random index in gameboard.phrases array
+    // ** assigns that array item to the phrase variable and splits it
+    // ** assigns that phrase to the gameboard.selectedPhrase key
+    // ** returns a split phrase
     getRandomPhrase: (arr) => {
         const phraseIndex = Math.floor(Math.random() * gameboard.phrases.length);
         phrase = gameboard.phrases[phraseIndex].split("");
         gameboard.selectedPhrase = phrase;
         return phrase;
     },
+    // addPhraseToDisplay function
+    // ** loops through the split (gameboard.selectedPhrase) and creates an li for each character
+    // ** if the li has a value, it adds a class of "letter" to it, otherwise adds a class of "space"
+    // ** appends newly created li to the ul within <div class="phrase-display"><ul>{here}</ul></div>
     addPhraseToDisplay: () => {
         const phraseDisplay = document.querySelector('.phrase-display ul');
         gameboard.selectedPhrase.forEach((char => {
@@ -72,9 +92,12 @@ const gameboard = {
     }   
 }
 
-
+// keyboard object
+// ** contains 1 function / handleClicks
 const keyboard = {
     currentClick: "",
+    // handle clicks ...handles the clicks on the keyboard
+    // contains the handleClickValidation function and the handleUpdatingHearts function
     handleClicks: () => {
         const keyboard = document.querySelector('.keyboard');
         keyboard.addEventListener('click', e => {
@@ -84,6 +107,7 @@ const keyboard = {
                 handleClickValidation();
             }
         });
+        // handleClickValidation 
         const handleClickValidation = () => {
             const phraseLetters = document.querySelectorAll('.phrase-display li');
             if (gameboard.selectedPhrase.includes(keyboard.currentClick)) {
@@ -98,6 +122,7 @@ const keyboard = {
                 scoreCheck.checkLoss();
             }
         }
+        // handleUpdatingHearts
         const handleUpdatingHearts = () => {
             gameboard.lives -= 1;
             const hearts = document.querySelectorAll('.heart-display img');
@@ -107,12 +132,15 @@ const keyboard = {
     }
 }
 
+// scoreCheck object
+// ** contains 2 function / checkWin and checkLoss
 const scoreCheck = {
     overlay:document.querySelector('.overlay'),
     heading: document.querySelector('.overlay h1'),
     p: document.querySelector('.overlay p'),
     btn:document.querySelector('.overlay button.overlay-btn'),
     keyboard: document.querySelector('.keyboard'),
+    // checkWin
     checkWin: () => {
         const totalLettersToGuess = document.querySelectorAll('.phrase-display ul li.letter').length;
         const totalLettersGuessed = document.querySelectorAll('.letter.show').length;
@@ -128,8 +156,8 @@ const scoreCheck = {
             }, 500)
         }
     },
+    // checkLoss
     checkLoss: () => {
-        
         if (gameboard.lives <= 0) {
             scoreCheck.keyboard.style.pointerEvents = 'none';
             scoreCheck.heading.textContent = `You didn't win, bummer!`;
@@ -143,34 +171,36 @@ const scoreCheck = {
     },
 }
 
-
+// restGame function
+// ** also makes calls to gameboardGetRandomPhrase & gameboard.addPhraseToDisplay
 const resetGame = () => {
+    // reset phrase display
     const phraseDisplay = document.querySelector('.phrase-display ul')
     phraseDisplay.innerHTML = '';
-
+    // reset keyboard UI
     const keyboard = document.querySelector('.keyboard');
     const keyboardKeys = keyboard.querySelectorAll('li');
-    
     keyboard.style.pointerEvents = 'all';
     keyboardKeys.forEach((key) => {
         key.classList.remove('clicked');
     });
-
+    // reset hearts
     const hearts = document.querySelectorAll('.heart-display img');
     hearts.forEach((img) => {
         img.classList.remove('lostheart');
         img.src = './assets/hearts/liveheart.png';
     });
-
+    // resets lives
     gameboard.lives = 5;
-
+    // gets a new random phrase, splits it, and appends to DOM
     gameboard.getRandomPhrase();
     gameboard.addPhraseToDisplay();
-
 }
 
-
-
+// initial function calls
+// starts game
 overlay.handleStart();
+// listens for keyboard(virtual) events
 keyboard.handleClicks();
+// handles theme switching
 theme.handleThemeToggle();
